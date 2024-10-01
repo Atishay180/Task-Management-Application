1f62176899935c80a98a7ef34eb926fd4e0e8f5a
Atishay180 <atishayjain8807@gmail.com>
2024-10-01 13:22:31 +0530

    Update API proxy target URL in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://task-management-application-z7kb.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
       }
     }

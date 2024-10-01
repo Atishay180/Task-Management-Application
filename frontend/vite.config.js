c302a27ed7b98b56de7fc189fab65cd02408b098
Atishay180 <atishayjain8807@gmail.com>
2024-10-01 12:48:34 +0530

    Update API proxy target URL and enable changeOrigin in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "/api": {
         // target: "http://localhost:5000",
         target: import.meta.env.VITE_API_URL,
+        changeOrigin: true,
       }
     }
   },
